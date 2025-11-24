2aebfeae10c883073f332d1794e398e7c094da79
Sid <sidviciousmbp@mac.lan>
2025-11-23 23:36:13 -0800

    A single index.html that contains the React App inline since we're not using a build tool like Webpack/Vite for this project yet

--- legacy/indexV2.jsx
+++ legacy/indexV2.jsx
@@ -610,3 +610,9 @@ const App = () => {
 };
 
 export default App;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App"; // or your main component
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
